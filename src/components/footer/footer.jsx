0bcd7208f33077730aa8acc0fc7f2381d0d144d7
Sagarchhipa <sagarchhipa7877702213@gmail.com>
2023-09-11 13:48:59 +0530

    abc

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    OverThinker is a React-App that is used for see informaion related to movies and web series like Overview, Rating, Runtime, Cast ,Genres, Trailer etc.It is use online database of informaion to show related to films, television series.
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
